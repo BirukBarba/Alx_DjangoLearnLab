19b6af1ab1a7154bfd75be0fc139e0d597318b3d
birukbarba <birukbarba@gmail.com>
2025-11-02 20:26:15 +0300

    edited

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # Add filtering options on the right sidebar
+    list_filter = ('author', 'publication_year')
+
+    # Add search functionality for these fields
+    search_fields = ('title', 'author')
 # Register your models here.
--- Introduction_to_Django/LibraryProject/bookshelf/delete.md
+++ Introduction_to_Django/LibraryProject/bookshelf/delete.md
@@ -8,10 +8,10 @@ from bookshelf.models import Book
 # Retrieve the book to delete
 book = Book.objects.get(title="Nineteen Eighty-Four")
 
-# Delete the book
+# Delete the book instance
 book.delete()
 
-# Confirm deletion by retrieving all books
+# Confirm deletion by retrieving all books again
 print(Book.objects.all())
 
 # Expected Output:
