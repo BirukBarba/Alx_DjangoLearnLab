4101de3e5691f4e2072221388af0525d8be2911c
birukbarba <birukbarba@gmail.com>
2025-11-07 14:17:25 +0300

    edited

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -68,3 +68,38 @@ def logout_view(request):
     logout(request)
     messages.info(request, "You have been logged out.")
     return render(request, 'relationship_app/logout.html')
+
+
+
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.shortcuts import render
+
+# --- Helper functions to check roles ---
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+# --- Role-Based Views ---
+
+@user_passes_test(is_admin)
+@login_required
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+@login_required
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+@login_required
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
