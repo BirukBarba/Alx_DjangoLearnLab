fa92c07bbc4c2cccc7e5b03e5fa9eeb0e1514582
birukbarba <birukbarba@gmail.com>
2025-12-14 20:49:32 +0300

    Sociale api added

---
+++ social_media_api/social_media_api/accounts/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from .serializers import (
+    RegisterSerializer,
+    LoginSerializer,
+    UserProfileSerializer
+)
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user': UserProfileSerializer(user).data
+        })
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+class ProfileView(APIView):
+    def get(self, request):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data)
