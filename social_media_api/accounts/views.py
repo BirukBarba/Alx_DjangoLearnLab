b7d98c168f44cf88c218e0cfa3c0f342fdc8f3a3
birukbarba <birukbarba@gmail.com>
2025-12-14 21:15:39 +0300

    Sociale api added

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -42,34 +42,36 @@ class ProfileView(APIView):
 
 
 
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from .models import User
+from .models import User 
+from .serializers import UserSerializer
 
-class FollowUserView(APIView):
-    permission_classes = [IsAuthenticated]
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         user_to_follow = get_object_or_404(User, id=user_id)
+        
         if user_to_follow == request.user:
             return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Add the user to the 'following' list
+        
         request.user.following.add(user_to_follow)
         return Response({"detail": f"You are now following {user_to_follow.username}."})
 
-
-class UnfollowUserView(APIView):
-    permission_classes = [IsAuthenticated]
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         user_to_unfollow = get_object_or_404(User, id=user_id)
+        
         if user_to_unfollow == request.user:
             return Response({"detail": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
         # Remove the user from the 'following' list
         request.user.following.remove(user_to_unfollow)
         return Response({"detail": f"You have unfollowed {user_to_unfollow.username}."})
+
